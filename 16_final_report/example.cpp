53b9119d615bf6c83ae66ad224153d1881a22a85
yumcyawiz <hashdolf1923@gmail.com>
2021-05-30 23:36:09 +0900

    beautify code

--- 16_final_report/example.cpp
+++ 16_final_report/example.cpp
@@ -1,8 +1,9 @@
 #include <mpi.h>
-#include <cstdio>
-#include <cmath>
-#include <vector>
+
 #include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <vector>
 using namespace std;
 
 int main(int argc, char** argv) {
@@ -12,64 +13,87 @@ int main(int argc, char** argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   const int N = 256;
-  vector<float> A(N*N);
-  vector<float> B(N*N);
-  vector<float> C(N*N, 0);
-  vector<float> subA(N*N/size);
-  vector<float> subB(N*N/size);
-  vector<float> subC(N*N/size, 0);
-  vector<float> recv(N*N/size);
-  for (int i=0; i<N; i++) {
-    for (int j=0; j<N; j++) {
-      A[N*i+j] = drand48();
-      B[N*i+j] = drand48();
+  vector<float> A(N * N);
+  vector<float> B(N * N);
+  vector<float> C(N * N, 0);
+  vector<float> subA(N * N / size);
+  vector<float> subB(N * N / size);
+  vector<float> subC(N * N / size, 0);
+  vector<float> recv(N * N / size);
+
+  // prepare A, B
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      A[N * i + j] = drand48();
+      B[N * i + j] = drand48();
     }
   }
-  int offset = N/size*rank;
-  for (int i=0; i<N/size; i++)
-    for (int j=0; j<N; j++)
-      subA[N*i+j] = A[N*(i+offset)+j];
-  for (int i=0; i<N; i++)
-    for (int j=0; j<N/size; j++)
-      subB[N/size*i+j] = B[N*i+j+offset];
+
+  // prepare sub array(distributed with MPI)
+  int offset = N / size * rank;
+  for (int i = 0; i < N / size; i++) {
+    for (int j = 0; j < N; j++) {
+      subA[N * i + j] = A[N * (i + offset) + j];
+    }
+  }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N / size; j++) {
+      subB[N / size * i + j] = B[N * i + j + offset];
+    }
+  }
   int recv_from = (rank + 1) % size;
   int send_to = (rank - 1 + size) % size;
 
   double comp_time = 0, comm_time = 0;
-  for(int irank=0; irank<size; irank++) {
+  for (int irank = 0; irank < size; irank++) {
     auto tic = chrono::steady_clock::now();
-    offset = N/size*((rank+irank) % size);
-    for (int i=0; i<N/size; i++)
-      for (int j=0; j<N/size; j++)
-        for (int k=0; k<N; k++)
-          subC[N*i+j+offset] += subA[N*i+k] * subB[N/size*k+j];
+    offset = N / size * ((rank + irank) % size);
+    for (int i = 0; i < N / size; i++) {
+      for (int j = 0; j < N / size; j++) {
+        for (int k = 0; k < N; k++) {
+          subC[N * i + j + offset] += subA[N * i + k] * subB[N / size * k + j];
+        }
+      }
+    }
     auto toc = chrono::steady_clock::now();
     comp_time += chrono::duration<double>(toc - tic).count();
     MPI_Request request[2];
-    MPI_Isend(&subB[0], N*N/size, MPI_FLOAT, send_to, 0, MPI_COMM_WORLD, &request[0]);
-    MPI_Irecv(&recv[0], N*N/size, MPI_FLOAT, recv_from, 0, MPI_COMM_WORLD, &request[1]);
+    MPI_Isend(&subB[0], N * N / size, MPI_FLOAT, send_to, 0, MPI_COMM_WORLD,
+              &request[0]);
+    MPI_Irecv(&recv[0], N * N / size, MPI_FLOAT, recv_from, 0, MPI_COMM_WORLD,
+              &request[1]);
     MPI_Waitall(2, request, MPI_STATUS_IGNORE);
-    for (int i=0; i<N*N/size; i++)
-      subB[i] = recv[i];
+    for (int i = 0; i < N * N / size; i++) subB[i] = recv[i];
     tic = chrono::steady_clock::now();
     comm_time += chrono::duration<double>(tic - toc).count();
   }
-  MPI_Allgather(&subC[0], N*N/size, MPI_FLOAT, &C[0], N*N/size, MPI_FLOAT, MPI_COMM_WORLD);
-  for (int i=0; i<N; i++)
-    for (int j=0; j<N; j++)
-      for (int k=0; k<N; k++)
-        C[N*i+j] -= A[N*i+k] * B[N*k+j];
+  MPI_Allgather(&subC[0], N * N / size, MPI_FLOAT, &C[0], N * N / size,
+                MPI_FLOAT, MPI_COMM_WORLD);
+
+  // evaluate error
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      for (int k = 0; k < N; k++) {
+        C[N * i + j] -= A[N * i + k] * B[N * k + j];
+      }
+    }
+  }
   double err = 0;
-  for (int i=0; i<N; i++)
-    for (int j=0; j<N; j++)
-      err += fabs(C[N*i+j]);
-  if(rank==0) {
-    double time = comp_time+comm_time;
-    printf("N    : %d\n",N);
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      err += fabs(C[N * i + j]);
+    }
+  }
+
+  // print info
+  if (rank == 0) {
+    double time = comp_time + comm_time;
+    printf("N    : %d\n", N);
     printf("comp : %lf s\n", comp_time);
     printf("comm : %lf s\n", comm_time);
-    printf("total: %lf s (%lf GFlops)\n",time,2.*N*N*N/time/1e9);
-    printf("error: %lf\n",err/N/N);
+    printf("total: %lf s (%lf GFlops)\n", time, 2. * N * N * N / time / 1e9);
+    printf("error: %lf\n", err / N / N);
   }
+
   MPI_Finalize();
 }
